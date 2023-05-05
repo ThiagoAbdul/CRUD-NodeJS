eb043f7cbc19ade665fde9e42e6bc491e6e6adfa
abdul <thiagoabdul10@gmail.com>
2023-05-04 22:16:02 -0300

    Server running

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config({path: 'variaveis.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+
+const port = process.env.PORT
+
+server.listen(port, () => console.log(`Server running at http://localhost:${port}`))
